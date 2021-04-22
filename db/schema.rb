36fbabe46a1fafaf5244bf5afc9f02fae8b4a6c5
Alamgir <ourandoc@gmail.com>
2021-04-22 08:05:43 +0500

    generate user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_22_013518) do
+ActiveRecord::Schema.define(version: 2021_04_22_022155) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.string "body"
   end
 
 end
