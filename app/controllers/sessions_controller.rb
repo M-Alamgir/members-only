ac3f20d9eebfaff0e3ee75836b18410084b7a05f
Alamgir <ourandoc@gmail.com>
2021-04-22 23:21:18 +0500

    fix linters

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to new_post_path
     else
