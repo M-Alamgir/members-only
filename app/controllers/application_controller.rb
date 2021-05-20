b9b39ba103b830de3bd27a77f49192efe1e6afc6
Alamgir <ourandoc@gmail.com>
2021-05-20 11:52:10 +0500

    Add name column to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
+	protect_from_forgery with: :exception
 
-  helper_method :current_user
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+        end
 end
