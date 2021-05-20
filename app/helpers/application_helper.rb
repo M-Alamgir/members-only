ac87758adcb18b1fcd7d6aadedb11512558ae8c5
Alamgir <ourandoc@gmail.com>
2021-05-20 22:52:02 +0500

    fix linter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,11 @@
 module ApplicationHelper
   def sign_in_check
     if current_user
-      "Hi #{current_user.name} | ".html_safe + link_to 'Log out', destroy_user_session_path,
-                                                          method: :delete.to_s.html_safe
+      "<p><span>Hi #{current_user.name} | </span>
+      #{link_to 'Log out', destroy_user_session_path, method: :delete}</p>".html_safe
     else
-
-      link_to 'log in',
-                 new_user_session_path.to_s.html_safe + ' | ' + link_to 'Sign up',
-                                                                        new_user_registration_path.to_s.html_safe
+      "<p>#{link_to 'log in', new_user_session_path}<span> | </span>
+      #{link_to 'Sign up', new_user_registration_path}</p>".html_safe
     end
   end
 end
