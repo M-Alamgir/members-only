cf5b7f37bae07fcffd992516cbe5cb2dbda81325
Alamgir <ourandoc@gmail.com>
2021-05-20 22:20:15 +0500

    fix linters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,13 @@
 module ApplicationHelper
-	def sign_in_check
-		if current_user
-      "Hi #{current_user.name} | ".html_safe + "#{link_to "Log out", destroy_user_session_path, method: :delete}".html_safe
+  def sign_in_check
+    if current_user
+      "Hi #{current_user.name} | ".html_safe + link_to 'Log out', destroy_user_session_path,
+                                                          method: :delete.to_s.html_safe
     else
-      
-      "#{link_to 'log in', new_user_session_path}".html_safe + ' | ' + "#{link_to "Sign up", new_user_registration_path }".html_safe
-      end
-	end
+
+      link_to 'log in',
+                 new_user_session_path.to_s.html_safe + ' | ' + link_to 'Sign up',
+                                                                        new_user_registration_path.to_s.html_safe
+    end
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,5 @@
 module PostsHelper
-	def show_name(post)
-		if current_user
-      "<p class='post-title'>posted by #{post.user.name}</p>".html_safe
-  	end
-	end
+  def show_name(post)
+    "<p class='post-title'>posted by #{post.user.name}</p>".html_safe if current_user
+  end
 end
